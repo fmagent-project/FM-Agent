31dfb2484b4bd7e2c66756bddffe16201b3ff4be
ED <ddqiaoyi@163.com>
2026-07-28 10:45:47 +0800

    feat(plugin): define domain context hook contracts

--- src/plugin.py
+++ src/plugin.py
@@ -106,6 +106,20 @@ STAGE_HOOK_CONTRACTS = {
             return_type=type(None),
         ),
     },
+    "generate_domain_context": {
+        "replace_function": HookContract(
+            parameter_types=(str, str, str),
+            return_type=list[str],
+        ),
+        "input_function": HookContract(
+            parameter_types=(str,),
+            return_type=type(None),
+        ),
+        "output_function": HookContract(
+            parameter_types=(str,),
+            return_type=type(None),
+        ),
+    },
     "extract_functions": {
         "replace_function": HookContract(
             parameter_types=(list[str], str),
